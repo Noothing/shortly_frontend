a4bfa69db0367cac7135f6b5fbd9e219803b683e
Edgard Reykh <68498236+IosifKarpov@users.noreply.github.com>
2022-11-19 23:33:12 +0500

    feat[Authorization]: Change api url

--- src/lib/Core/Authorization.svelte
+++ src/lib/Core/Authorization.svelte
@@ -4,19 +4,84 @@
      * Svelte
      */
     import {writable} from "svelte/store";
-    import {setContext} from "svelte";
+    import {getContext, onMount, setContext} from "svelte";
+    import axios from "axios";
+
+    /**
+     * Get context
+     */
+    const {getCookie, setCookie, deleteCookie} = getContext("cookies")
 
     /**
      * Store
      */
-    const isAuth = writable(true)
+    const isAuth = writable(false)
+    const user = writable({})
 
     /**
      * Set context
      */
+    const baseUrl = "https://api.shorlty.tech/"
+
+    const api = writable(
+        axios.create({
+            withCredentials: true,
+            baseURL: baseUrl,
+            headers: {
+                "Content-Type": "application/json;charset=UTF-8",
+                "Access-Control-Allow-Origin": "*",
+            }
+        })
+    )
+
+    let timeout
+
+    const refresh = async () => {
+        clearTimeout(timeout)
+        try {
+            const {data: req} = await $api.get("/user/refresh")
+            if (req.success) {
+                let refreshTime = req.data.info.expire_at - Math.round(Date.now() / 1000)
+                timeout = setTimeout(refresh, refreshTime * 1000)
+                $isAuth = true
+
+                $user = req.data.user
+            } else {
+                $isAuth = false
+            }
+        } catch (e) {
+            $isAuth = false
+            console.log('[AUTH]', e)
+        }
+    }
+
+    const logout = async () => {
+        const {data: req} = await $api.get("/user/logout")
+        $isAuth = false
+    }
+
+    const register = async (data) => {
+        try {
+            const {data: req} = await $api.post('/user/registration', data)
+            return req
+        } catch (e) {
+            console.log('[REGISTER]', e)
+            return false
+        }
+    }
+
     setContext('isAuth', isAuth)
+    setContext("baseURL", baseUrl)
+    setContext('checkAuth', refresh)
+    setContext("api", api)
+    setContext("logout", logout)
+    setContext("user", user)
+    setContext("register", register)
 
-
+    $: console.log('isAuth', $isAuth)
+    $: console.log('user', $user)
 </script>
 
-<slot/>
+{#await refresh() then }
+    <slot/>
+{/await}
